dd9f7d4ff1df4af49380ae2bcede5d03ea42560c
roc <rocsalvador001@protonmail.com>
2022-04-29 12:47:16 +0200

    millores en l script

--- jsbach.sh
+++ jsbach.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# -lt 1 ]; then
+    echo Usage:
+    echo ./jsbach.sh file.jsb [outFileName]
+    exit 1
+fi
 
 if [ "$2" == "" ]; then
     OUTFILE=default
@@ -10,14 +15,11 @@ if [ ! -d out ]; then
     mkdir out 
 fi
 
-antlr4 -Dlanguage=Python3 -no-listener -visitor src/Expr.g
-
-python3 src/jsbach.py $1 out/$OUTFILE
-
-lilypond out/$OUTFILE.lily
-
-timidity -Ow -o $OUTFILE.wav $OUTFILE.midi
-
-mv $OUTFILE.wav $OUTFILE.pdf $OUTFILE.midi -t out
-
-ffplay -nodisp -autoexit out/$OUTFILE.wav 
+if antlr4 -Dlanguage=Python3 -no-listener -visitor src/Expr.g &&
+   python3 src/jsbach.py $1 out/$OUTFILE && 
+   lilypond out/$OUTFILE.lily
+then
+    timidity -Ow -o $OUTFILE.wav $OUTFILE.midi
+    mv $OUTFILE.wav $OUTFILE.pdf $OUTFILE.midi -t out
+    ffplay -nodisp -autoexit out/$OUTFILE.wav 
+fi
